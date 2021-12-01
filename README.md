60ad49bbd2c41d4dbcf4ea3344dc8923fc3e9a4b
Daniël Goossens <daniel@goossens.ch>
2021-12-01 17:22:11 +0100

    Initial commit - Day 1

---
+++ README.md
@@ -0,0 +1,4 @@
+Advent of Code 2021
+---
+
+My solutions for the [Advent of Code](https://adventofcode.com/) in 2021.
---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,44 @@
+import java.io.BufferedReader
+import java.io.FileReader
+import java.nio.file.Paths
+
+/**
+ * Day 1 - Sonar Sweep
+ */
+fun main() {
+    val measurements = mutableListOf<Int>()
+    BufferedReader(FileReader(Paths.get("1-1.txt").toFile())).use {
+        while (it.ready()) {
+            measurements += it.readLine().toInt()
+        }
+    }
+
+    puzzle1(measurements)
+    puzzle2(measurements)
+}
+
+fun puzzle1(measurements: List<Int>) {
+    var increments = 0
+    for (index in 1 until measurements.size) {
+        val previous = measurements[index - 1]
+        val current = measurements[index]
+
+        if (current > previous) {
+            increments++
+        }
+    }
+    println("P1 Increments: $increments")
+}
+
+fun puzzle2(measurements: List<Int>) {
+    var increments = 0
+    for (index in 1 until measurements.size - 2) {
+        val previous = measurements[index - 1] + measurements[index] + measurements[index + 1]
+        val current = measurements[index] + measurements[index + 1] + measurements[index + 2]
+
+        if (current > previous) {
+            increments++
+        }
+    }
+    println("P2 Increments: $increments")
+}
