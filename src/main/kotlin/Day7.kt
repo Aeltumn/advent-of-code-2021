f74cecba6da0004fc5ec29fb076bd28081eb6cff
Daniël Goossens <daniel@goossens.ch>
2021-12-07 23:15:20 +0100

    Day 7!
    This one was pretty fast to do

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,24 @@
+import java.io.File
+import kotlin.math.abs
+
+/**
+ * Day 7 - The Treachery of Whales
+ */
+fun main() {
+    File("day7").readLines()[0].split(",").map { it.toInt() }.also { crabs ->
+        val min = crabs.minOrNull() ?: return
+        val max = crabs.maxOrNull() ?: return
+
+        println("P1 Optimal fuel usage: ${(min..max).minOfOrNull { target -> crabs.sumOf { abs(it - target) } }}")
+        println(
+            "P2 Optimal fuel usage: ${
+                (min..max).minOfOrNull { target ->
+                    crabs.sumOf {
+                        val distance = abs(it - target)
+                        distance * (distance + 1) / 2
+                    }
+                }
+            }"
+        )
+    }
+}
