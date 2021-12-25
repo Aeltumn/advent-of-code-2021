77fc0a32c167e7b6ab07ad9193f8ac2a9b54492c
Daniël Goossens <daniel@goossens.ch>
2021-12-25 01:12:33 +0100

    Day 16 & 17!

---
+++ src/main/kotlin/Day16.kt
@@ -0,0 +1,108 @@
+import java.io.File
+
+/** Returns the string representation of this number in the given radix padded to be length long. */
+private fun Int.toPaddedString(radix: Int, length: Int): String {
+    val result = toString(radix)
+    return "0".repeat(length - result.length) + result
+}
+
+/**
+ * Day 16 - Packet Decoder
+ */
+fun main() {
+    // The input represented in bits stored as chars, I could keep it as an actual int but I prefer
+    // checking chars over dealing with bit manipulation in Kotlin.
+    val input = File("day16").readLines()[0].lowercase().flatMap { it.digitToInt(16).toPaddedString(2, 4).toCharArray().toList() }
+    var head = 0
+    var totalVersion = 0
+
+    /**
+     * Reads an int from the binary stream starting at head for [length].
+     */
+    fun readValue(length: Int): Int {
+        var str = ""
+        for (i in 0 until length) {
+            str += input[i + head]
+        }
+        head += length
+        return str.toInt(2)
+    }
+
+    /**
+     * Reads a packet starting at  the given index.
+     */
+    fun readPacket(): Long {
+        val version = readValue(3)
+        totalVersion += version
+
+        val type = readValue(3)
+        if (type == 4) {
+            // Literal value
+            var literal = ""
+            var readLastChunk = false
+            while (!readLastChunk) {
+                val next = readValue(5).toPaddedString(2, 5)
+
+                // If this chunk starts with a 0 it ends here
+                if (next[0] == '0') {
+                    readLastChunk = true
+                }
+
+                // Add this chunk to the value (ignore first bit)
+                literal += next.substring(1)
+            }
+            return literal.toLong(2)
+        } else {
+            // Operator
+            val mode = readValue(1)
+            val values = mutableListOf<Long>()
+
+            if (mode == 0) {
+                // 15 bits are the length of upcoming packets
+                val length = readValue(15)
+                val end = head + length
+                while (head < end) {
+                    values += readPacket()
+                }
+                require (head == end) { "Reading packets in operator did not land at the correct spot" }
+            } else {
+                // 11 bits are a number of packets
+                val amount = readValue(11)
+                for (i in 0 until amount) {
+                    values += readPacket()
+                }
+            }
+
+            var result = 0L
+            when(type) {
+                0 -> result = values.sum()
+                1 -> {
+                    result = 1
+                    for (value in values) {
+                        result *= value
+                    }
+                }
+                2 -> result = values.minOrNull() ?: 0
+                3 -> result = values.maxOrNull() ?: 0
+                5 -> {
+                    require (values.size == 2) { "Greater than only supports two subpackets" }
+                    return if (values[0] > values[1]) 1 else 0
+                }
+                6 -> {
+                    require (values.size == 2) { "Lesser than only supports two subpackets" }
+                    return if (values[0] < values[1]) 1 else 0
+                }
+                7 -> {
+                    require (values.size == 2) { "Equal to only supports two subpackets" }
+                    return if (values[0] == values[1]) 1 else 0
+                }
+            }
+            return result
+        }
+    }
+
+    // Read out the packet
+    val result = readPacket()
+    println("P1 $totalVersion")
+    println("P2 $result")
+}
---
+++ src/main/kotlin/Day17.kt
@@ -0,0 +1,83 @@
+import java.io.File
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sign
+
+/**
+ * Day 17 - Trick Shot
+ */
+fun main() {
+    val input = File("day17").readLines()[0].substringAfter("target area: ").split(", ")
+    val targetXs = input[0].substringAfter("x=").split("..")
+    val targetYs = input[1].substringAfter("y=").split("..")
+
+    // Determine the target area
+    val minX = targetXs[0].toInt()
+    val maxX = targetXs[1].toInt()
+    val minY = targetYs[0].toInt()
+    val maxY = targetYs[1].toInt()
+
+    // Determine the shot strength
+    var x = 0
+    var y = 0
+    var xVelocity = 0
+    var yVelocity = 0
+    var highestY = 0
+
+    /**
+     * Runs one step of the simulation. Returns true if we've reached the target area.
+     */
+    fun runStep(): Boolean {
+        // Move the probe
+        x += xVelocity
+        y += yVelocity
+
+        // Update highest y
+        highestY = highestY.coerceAtLeast(y)
+
+        // Drag reduces the x velocity by 1 and gravity reduces y velocity by 1
+        xVelocity = (xVelocity - 1).coerceAtLeast(0)
+        yVelocity -= 1
+
+        // Return whether we've reached the goal
+        return x in minX..maxX && y in minY..maxY
+    }
+
+    /**
+     * Simulates the trajectory with the given x and y velocity. Returns whether it hit the target.
+     */
+    fun runSimulation(velocityX: Int, velocityY: Int): Boolean {
+        x = 0
+        y = 0
+        highestY = 0
+        xVelocity = velocityX
+        yVelocity = velocityY
+
+        // Keep running steps until we're either hopeless or we're done
+        while(!runStep()) {
+            // If we've hopelessly missed we stop
+            if (xVelocity.sign != (if (x < minX) 1 else -1) && y < minY) {
+                return false
+            }
+        }
+        return true
+    }
+
+    // Test out different velocities
+    // This is far from optimal of course, but the code is so fast that it really doesn't matter.
+    var record = 0
+    var valid = 0
+
+    val bound = max(max(abs(minX * 2), abs(minY * 2)), max(abs(maxX * 2), abs(maxY * 2)))
+    for (vx in -bound..bound) {
+        for (vy in -bound..bound) {
+            if (runSimulation(vx, vy)) {
+                record = record.coerceAtLeast(highestY)
+                valid++
+            }
+        }
+    }
+
+    println("P1 $record")
+    println("P2 $valid")
+}
